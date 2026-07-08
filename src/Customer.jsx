531b2bdf1aa588a3689132c635377b35df26562a
vipasha3 <vipasha.khokhar089@gmail.com>
2026-07-08 15:17:11 +0530

    Fix profile update issue

--- src/Customer.jsx
+++ src/Customer.jsx
@@ -24,6 +24,7 @@ export default function Customer() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [profile, setProfile] = useState({ 
     name: '', 
     phone: '', 
